02dc8b45dcddc6531e32217f9840ad1a0b6b5196
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-04 16:44:04 +0530

    IE Support

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,19 +4,17 @@ import './App.css';
 
 function App() {
   return (
+    /**  Test Comment **/ 
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello React
         </a>
       </header>
     </div>
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
