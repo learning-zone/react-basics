f3562803be03db3872bcd438cc9815485f5db6d4
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-05 10:03:26 +0530

    SCSS Conversion

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,7 +2,7 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
