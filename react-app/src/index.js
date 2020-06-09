8b62ccaf7d81075c1bd6c9e8370e19ee5c9fb7bb
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-09 08:48:19 +0530

    Timer

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const name = 'React JS';
-const element = <h1>Hello, {name}</h1>;
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
