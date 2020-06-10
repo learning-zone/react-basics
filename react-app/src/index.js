ef4b93ec18c86ddda0ec0ac93ce68c7d2aa8be29
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-10 15:31:02 +0530

    React Class

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
-const element = <Welcome name="Pradeep" />;
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date()} />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
