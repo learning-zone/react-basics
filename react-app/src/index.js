56b77d3fefc043449008c89f3f8993de4d7cffab
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-10 14:46:25 +0530

    Component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-setInterval(tick, 1000);
+const element = <Welcome name="Pradeep" />;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
