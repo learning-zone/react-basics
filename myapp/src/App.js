3b57292884cd67880aaa3ddf76c056391af32a0c
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-13 17:49:15 +0530

    Component Nesting

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import Ninjas  from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1},
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2},
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3},
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React App!</h1>
         <p>Welcome :)</p>
-        <Ninjas />
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
 class Ninjas extends Component {
+    
     render() {
+        const { ninjas } = this.props;
+        const ninjaList = ninjas.map(ninja => {
+            return (
+                <div className="ninja" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div><br/>
+                </div>
+            )
+        })
         return (
-            <div className="ninja">
-                <div>Name: Ryu</div>
-                <div>Age: 20</div>
-                <div>Belt: Black</div>
+            <div className="ninja-list">
+                <div>{ ninjaList }</div>
             </div>
         )
     }
