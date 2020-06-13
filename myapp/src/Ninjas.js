9bb113c181e5683765d53100b934c3a3ef5f53f8
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-13 22:00:24 +0530

    Conditional Operation

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
+const Ninjas = ({ninjas}) => {
     
-    render() {
-        const { ninjas } = this.props;
         const ninjaList = ninjas.map(ninja => {
-            return (
+            return ninja.age > 20 ? (
                 <div className="ninja" key={ninja.id}>
                     <div>Name: {ninja.name}</div>
                     <div>Age: {ninja.age}</div>
                     <div>Belt: {ninja.belt}</div><br/>
                 </div>
-            )
-        })
+            ) : null;
+        });
         return (
             <div className="ninja-list">
                 <div>{ ninjaList }</div>
             </div>
         )
     }
-}
 
 export default Ninjas
