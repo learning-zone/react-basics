f8e9682257725e79995a46a98f616380f191b34c
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-14 22:35:21 +0530

    HOC in React

--- poketimes/src/components/About.js
+++ poketimes/src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rainbow from '../hoc/Rainbow';
 
 const About = () => {
     return (
@@ -8,4 +9,4 @@ const About = () => {
         </div>
     )
 }
-export default About
+export default Rainbow(About);
