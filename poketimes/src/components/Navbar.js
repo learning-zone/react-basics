ce08108bd7e42e8a0518c4ae4262aa9d8901a712
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-14 21:26:39 +0530

    React Router

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -6,9 +7,9 @@ const Navbar = () => {
             <div className="container">
                 <a className="brand-logo">Poke'Times</a>
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
 
                 </ul>
             </div>
