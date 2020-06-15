051b834a9d58c6c6b97fa2949f0f68e72cd1eaf7
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-15 21:27:22 +0530

    Redux and Dispatch

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Post extends Component {
+    handleClick = () => {
+        this.props.deletePost(this.props.post.id);
+        this.props.history.push('/');
+    }
     render() {
+        console.log(this.props);
         const post = this.props.post ? (
             <div className="post">
                 <h4 className="center">{this.props.post.title}</h4>
                 <p>{this.props.post.body}</p>
+                <div className="center">
+                  <button className="btn grey" onClick={this.handleClick}>
+                      Delete Post
+                  </button>
+                </div>
             </div>
         ) : (
             <div className="center">Loading post...</div>
@@ -21,9 +31,13 @@ class Post extends Component {
 
 const mapStateToProps = (state, ownProps) => {
     let id = ownProps.match.params.post_id;
-    console.log('id: ' + id);
     return {
         post: state.posts.find(post => post.id == id)
     }
 }
-export default connect(mapStateToProps)(Post);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => { dispatch({type: 'DELETE_POST', id: id}) }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -19,6 +19,15 @@ const initState = {
 }
 
 const rootReducer = (state = initState, action) => {
+    if(action.type === 'DELETE_POST') {
+        let newPosts = state.posts.filter(post => {
+            return action.id !== post.id
+        });
+        return {
+            ...state,
+            posts: newPosts
+        }
+    }
     return state;
 }
 export default rootReducer;
