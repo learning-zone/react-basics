21a84b71e898766df8321e40c3ffaf71a34cec17
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-15 18:06:40 +0530

    Redux

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Post extends Component {
-    state = {
-        post: null
-    }
-    componentDidMount() {
-        let id = this.props.match.params.post_id;
-        axios.get('https:jsonplaceholder.typicode.com/posts/' + id)
-           .then(res => {
-              this.setState({
-                  post: res.data
-              })
-           })
-    }
     render() {
-
-        const post = this.state.post ? (
+        const post = this.props.post ? (
             <div className="post">
-                <h4 className="center">{this.state.post.title}</h4>
-                <p>{this.state.post.body}</p>
+                <h4 className="center">{this.props.post.title}</h4>
+                <p>{this.props.post.body}</p>
             </div>
         ) : (
             <div className="center">Loading post...</div>
@@ -31,4 +18,12 @@ class Post extends Component {
         )
     }
 }
-export default Post;
+
+const mapStateToProps = (state, ownProps) => {
+    let id = ownProps.match.params.post_id;
+    console.log('id: ' + id);
+    return {
+        post: state.posts.find(post => post.id == id)
+    }
+}
+export default connect(mapStateToProps)(Post);
