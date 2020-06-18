443aef9c8167611d1bdb7113bbfd7459a1e39903
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-18 10:25:30 +0530

    React App

---
+++ react-express-mysql/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Home Page
+      </div>
+    );
+  }
+}
+
+export default App;
