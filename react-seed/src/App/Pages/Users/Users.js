216c6f3c98b40d60c64fafc6fd2c3b95622a170a
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-23 17:55:08 +0530

    Progress Indicator

--- react-seed/src/App/Pages/Users/Users.js
+++ react-seed/src/App/Pages/Users/Users.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Table from "./Table";
+import './Users.scss';
 
 
 export default class Users extends React.Component {
+  
   state = {
+    hide: '',
     result: [],
     column_name: [
       { title: "Name", field: "name" },
@@ -16,15 +21,26 @@ export default class Users extends React.Component {
     axios.get(`/api/get/user`)
       .then(res => {
         const result = res.data.recordset;
+        const hide = 'hide';
         this.setState({ result });
+        this.setState({ hide });
       })
   }
 
   render() {
+
     return (
-     <div className="Users">
-      <Table col={this.state.column_name} data={this.state.result} />
-     </div>
+        <div className="Users" style={{position: 'relative'}}>
+          <CircularProgress 
+              className={this.state.hide}
+              color="secondary" 
+              size={40}
+              left={-20}
+              top={10}
+              status={'loading'}
+              disableShrink={this.state.loading} />
+          <Table col={this.state.column_name} data={this.state.result} />
+        </div>
     )
   }
 }
