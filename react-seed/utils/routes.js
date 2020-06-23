9d47aa1578e051e39e1d74f37a96210cf52ca97d
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-23 16:15:32 +0530

    Update routes.js

--- react-seed/utils/routes.js
+++ react-seed/utils/routes.js
@@ -5,7 +5,7 @@ const config = require("./config");
 
 
 /**
- * SQL-Server Connection Configuration
+ * API to GET User Details
  */
 router.get('/api/sitename', function (req, res, next) {
    
@@ -23,6 +23,39 @@ router.get('/api/sitename', function (req, res, next) {
                         ,[SiteURL]
                         ,[ModificationTime]
                     FROM [BFEnterprise].[dbo].[SITENAMEMAP] ORDER BY [Sitename]`;
+
+        request.query(stmt, function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+
+/**
+ * API to ADD User Details
+ */
+router.post('/api/post/user', function (req, res, next) {
+   
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+
+        console.log(req.body);
+         
+           
+        // query to the database and get the records
+        request.input('name', sql.VarChar, req.body.userDetails.name);
+        request.input('email', sql.VarChar, req.body.userDetails.email);
+        const stmt = `INSERT INTO [BFEnterprise].[webui].[TEST_USERS] (name, email) VALUES (@name, @email)`;
 
         request.query(stmt, function (err, recordset) {
             
