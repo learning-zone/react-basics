1d8aa6dfbcc747bee5024aea126a6bb8f9d3b9b7
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-07-01 22:58:25 +0530

    Login Form

---
+++ react-login/src/App/Components/LoginForm.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import './LoginForm.scss';
+
+
+class LoginForm extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      currentView: "signUp"
+    }
+  }
+
+  changeView = (view) => {
+    this.setState({
+      currentView: view
+    })
+  }
+
+  currentView = () => {
+    switch(this.state.currentView) {
+      case "signUp":
+        return (
+          <form>
+            <h2>Sign Up!</h2>
+            <fieldset>
+              <legend>Create Account</legend>
+              <ul>
+                <li>
+                  <label htmlFor="username">Username:</label>
+                  <input type="text" id="username" required/>
+                </li>
+                <li>
+                  <label htmlFor="email">Email:</label>
+                  <input type="email" id="email" required/>
+                </li>
+                <li>
+                  <label htmlFor="password">Password:</label>
+                  <input type="password" id="password" required/>
+                </li>
+              </ul>
+            </fieldset>
+            <button>Submit</button>
+            <button type="button" onClick={ () => this.changeView("logIn")}>Have an Account?</button>
+          </form>
+        )
+        break
+      case "logIn":
+        return (
+          <form>
+            <h2>Welcome Back!</h2>
+            <fieldset>
+              <legend>Log In</legend>
+              <ul>
+                <li>
+                  <label htmlFor="username">Username:</label>
+                  <input type="text" id="username" required/>
+                </li>
+                <li>
+                  <label htmlFor="password">Password:</label>
+                  <input type="password" id="password" required/>
+                </li>
+                <li>
+                  <i/>
+                  <a onClick={ () => this.changeView("PWReset")} href="#">Forgot Password?</a>
+                </li>
+              </ul>
+            </fieldset>
+            <button>Login</button>
+            <button type="button" onClick={ () => this.changeView("signUp")}>Create an Account</button>
+          </form>
+        )
+        break
+      case "PWReset":
+        return (
+          <form>
+          <h2>Reset Password</h2>
+          <fieldset>
+            <legend>Password Reset</legend>
+            <ul>
+              <li>
+                <em>A reset link will be sent to your inbox!</em>
+              </li>
+              <li>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" required/>
+              </li>
+            </ul>
+          </fieldset>
+          <button>Send Reset Link</button>
+          <button type="button" onClick={ () => this.changeView("logIn")}>Go Back</button>
+        </form>
+        )
+      default:
+        break
+    }
+  }
+
+
+  render() {
+    return (
+      <section id="entry-page">
+        {this.currentView()}
+      </section>
+    )
+  }
+}
+
+export default LoginForm;
--- react-login/src/App/Components/Routes.js
+++ react-login/src/App/Components/Routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Login from './Login';
+import LoginForm from './LoginForm';
 import Admin from './Admin';
 import Logout from './Logout';
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact={true} path='/' component={Login} />
+      <Route exact={true} path='/' component={LoginForm} />
       <Route path='/admin' component={Admin} />
       <Route path='/logout' component={Logout} />
     </Switch>
