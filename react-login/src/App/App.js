fc79b3f7f060d1e8eb10abdd258e4a04b445b7cf
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-07-03 23:01:34 +0530

    Header

--- react-login/src/App/App.js
+++ react-login/src/App/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import Routes from './Components/Routes';
+
+import Header from './Components/Header/Header';
+import Routes from './Components/Routes/Routes';
 
 const history = createBrowserHistory()
 
 function App() {
     return (
       <Router history={history}>
+        <Header />
         <Routes />
       </Router>
     );
