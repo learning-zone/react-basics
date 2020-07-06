17a4a9b8738367a8d63719fe0bd1376a72d37d5d
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-07-06 16:44:13 +0530

    Home Page

--- react-login/src/App/Pages/Home/Home.js
+++ react-login/src/App/Pages/Home/Home.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-export default class Admin extends Component {
+export default class Home extends Component {
     render() {
         return (
             <div>
-                Admin Page !
+                Home Page !
             </div>
         )
     }
