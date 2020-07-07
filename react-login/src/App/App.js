9af0544a31591d319c32a21a6b81b932f72e7110
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-07-07 12:32:21 +0530

    Header moved to Router Page

--- react-login/src/App/App.js
+++ react-login/src/App/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
-import Header from './Components/Header/Header';
 import Routes from './Components/Routes/Routes';
 
 const history = createBrowserHistory()
 
+
 function App() {
-    return (
-      <Router history={history}>
-        <Header />
-        <Routes />
-      </Router>
-    );
+
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
+ 
 }
 export default App;
