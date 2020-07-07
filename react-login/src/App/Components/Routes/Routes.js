84492e0c4296443b179e659dcd9bb1eed8794b52
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-07-07 12:33:33 +0530

    Header Logic

--- react-login/src/App/Components/Routes/Routes.js
+++ react-login/src/App/Components/Routes/Routes.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { withRouter, Route, Switch } from 'react-router-dom';
 
+import Home from '../../Pages/Home/Home';
 import UserDetails from '../../Pages/UserDetails/UserDetails';
 import UserForm from '../../Pages/UserForm/UserForm';
 import LoginForm from '../../Pages/LoginForm/LoginForm';
 import Logout from '../../Pages/Logout/Logout';
+import Header from '../Header/Header';
 
+let header;
 
 const Routes = () => {
+
+  // Hide Header from Login Page
+  header = (window.location.pathname === '/') ? '' : <Header />;
   return (
+    <div>
+     {header}
     <Switch>
-        <Route exact={true} path='/' component={LoginForm} />
+        <Route exact={true} path="/" component={LoginForm} />
+        <Route exact={true} path="/home" component={Home} />
         <Route exact={true} path="/user-form" component={UserForm} />
         <Route exact={true} path="/user-details" component={UserDetails} />
-        <Route exact={true} path='/logout' component={Logout} />
+        <Route exact={true} path="/logout" component={Logout} />
     </Switch>
+    </div>
   );
 }
 
-export default Routes;
+export default withRouter(Routes);
