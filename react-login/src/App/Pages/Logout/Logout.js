1070f5b6d40b2f2c1e37e80a1c256c8ff48f7c11
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-07-07 12:34:18 +0530

    Logout Page

--- react-login/src/App/Pages/Logout/Logout.js
+++ react-login/src/App/Pages/Logout/Logout.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { Redirect } from "react-router-dom";
 
 export default class Logout extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+          formData: {
+            login: false,
+            store: ''
+          }
+        }
+    }
+    componentDidMount() {
+      // Clear User Session
+      sessionStorage.setItem('login', JSON.stringify({
+        login: false,
+        store: ''
+      }));
+      sessionStorage.clear();
+      this.setState({login: false, store: ''});
+    }
+
     render() {
-        return (
-            <div>
-                Logout Page !
-            </div>
-        )
+      /**
+       * Redirect to Login Page
+       */
+      if (this.state.login === false) {
+          return <Redirect to="/" />
+      }
+      return (
+          <div>
+              Redirecting to Login...
+          </div>
+      )
     }
 }
