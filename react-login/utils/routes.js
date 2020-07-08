63cedda7eff1e0f6cfe57836143490f7c91ca7b5
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-07-08 16:31:29 +0530

    Database Routes

--- react-login/utils/routes.js
+++ react-login/utils/routes.js
@@ -2,23 +2,50 @@ const express = require('express');
 const router = express.Router();
 const sql = require('mssql');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto')
 const config = require('./config');
 const verifyToken = require('./verifyToken');
-
+const { password } = require('./config');
 
 /**
  * USER LOGIN 
  */
-router.post('/api/post/login', (req, res) => {
-    // Verify the user details from DB 
+router.post('/api/post/login', function(req, res, next) {
+
+    // MD5() for hashing
+    let hash = crypto.createHash('md5').update(req.body.formData.password).digest("hex")
+    
     const user = {
-        email: req.body.formData.email || 'kumar_prad@hcl.com',
-        password: req.body.formData.password || 'root'
+        email: req.body.formData.email,
+        password: hash
     }
-    // Reference: https://github.com/auth0/node-jsonwebtoken
-    jwt.sign({user}, 'secretkey', { expiresIn: '60s' }, (err, token) => {
-        res.json({
-            token: token
+
+    // Verify the user details from DB
+    sql.connect(config, function(err){
+        if(err) console.log(err)
+
+        var request = new sql.Request();
+
+        request.input('email', sql.VarChar, req.body.formData.email);
+        request.input('password', sql.VarChar, hash);
+
+        const smt = `SELECT COUNT(id)
+            FROM [BFEnterprise].[webui].[TEST_USERS]
+            WHERE [email] = @email AND password = @password`;
+
+        request.query(smt, function(err, recordset) {
+            if(err) console.log(err)
+
+            if(recordset.rowsAffected > 0) {
+            
+                // Reference: https://github.com/auth0/node-jsonwebtoken
+                jwt.sign({user}, 'secretkey', { expiresIn: '1h' }, (err, token) => {
+                    res.json({
+                        accessToken: token
+                    });
+                });
+            }
+            //res.send(recordset);
         });
     });
 });
@@ -45,9 +72,8 @@ router.get('/api/get/user', verifyToken, function(req, res, next) {
     
             request.query(smt, function(err, recordsets) {
                 if(err) console.log(err)
-    
-                res.send(recordsets);
                 console.log('authData: '+authData);
+                res.send(recordsets);
             });
         });
       }
@@ -57,23 +83,33 @@ router.get('/api/get/user', verifyToken, function(req, res, next) {
 /**
  * USER POST REQUEST
  */
-router.post('/api/post/user', function(req, res, next) {
+router.post('/api/post/user', verifyToken, function(req, res, next) {
 
-    sql.connect(config, function(err){
-        if(err) console.log(err)
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+          res.sendStatus(403);
+        } else {
 
-        var request = new sql.Request();
-
-        request.input('name', sql.VarChar, req.body.formData.name);
-        request.input('email', sql.VarChar, req.body.formData.email);
-
-        const smt = `INSERT INTO [BFEnterprise].[webui].[TEST_USERS] (name, email) VALUES (@name, @email)`;
-
-        request.query(smt, function(err, recordset) {
-            if(err) console.log(err)
-
-            res.send(recordset);
-        });
+           sql.connect(config, function(err){
+               if(err) console.log(err)
+       
+               var request = new sql.Request();
+               let password = '63a9f0ea7bb98050796b649e85481845';
+       
+               request.input('name', sql.VarChar, req.body.formData.name);
+               request.input('email', sql.VarChar, req.body.formData.email);
+               request.input('password', sql.VarChar, password);
+       
+               const smt = `INSERT INTO [BFEnterprise].[webui].[TEST_USERS] (name, email, password) VALUES (@name, @email, @password)`;
+       
+               request.query(smt, function(err, recordset) {
+                   if(err) console.log(err)
+                   console.log('authData: '+JSON.stringify(authData));
+                   res.send(recordset);
+                   
+               });
+           });
+        }
     });
 });
 
